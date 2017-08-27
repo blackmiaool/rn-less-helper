badf22ec64ac5534d511e28106288671b3afe9cd
blackmiaool <blackmiaool@gmail.com>
2017-08-27 21:49:28 +0800

    it works

---
+++ extension.js
@@ -0,0 +1,156 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+
+const vscode = require('vscode');
+const {
+    Uri,
+    TextDocument,
+    Position,
+    CancellationToken,
+    Range,
+    Location,
+    Definition,
+    MarkedString
+} = vscode;
+const Path = require('path');
+const fs = require('fs');
+const postcss = require('postcss');
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+function activate(context) {
+    const rootPath = vscode.workspace.rootPath;
+    const provider = new RnLessDefinitionProvider();
+    vscode.languages.registerHoverProvider(['javascriptreact'],provider);
+    vscode.languages.registerDefinitionProvider(['javascriptreact'], provider);
+
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {}
+exports.deactivate = deactivate;
+
+
+class RnLessDefinitionProvider {
+    constructor() {
+        // this.generateDefinitionMap();
+    }
+    async provideHover(document, position, token) {
+        const definition = await this.provideDefinition(document, position, token);
+        const space=definition.raw.split('\n')[1].match(/\s+/)[0].slice(4);
+        return new vscode.Hover([{language:'less',value:definition.raw.split(space).join('')}]);
+    }
+    provideDefinition(document, position, token) {
+        const currWordRange = document.getWordRangeAtPosition(position);
+        // Ensure the current word is valid
+        if (!currWordRange) {
+            return;
+        }
+        try {
+            let lineText = document.lineAt(position.line).text;
+            const currWord = document.getText(currWordRange);
+            const currWordBig = document.getText(new Range(currWordRange.start.translate(0, -1), currWordRange.end.translate(0, 1)));
+            if (currWordBig !== `"${currWord}"` && currWordBig !== `'${currWord}'`) {
+                return; //not a string
+            }
+            const codeBeforeWord = document.getText(new Range(new Position(0, 0), position));
+            let currentClass = codeBeforeWord.match(/([$\w]+)\)\s*\nclass\s+([$\w]+)/g);
+
+            if (!currentClass) {
+                return;
+            } else {
+                currentClass = currentClass[currentClass.length - 1].match(/^([$\w]+)/)[1];
+            }
+            let lessFiles = codeBeforeWord.match(/(["'])[^\1\n={}]+\.less\1/g);
+            if (!lessFiles) {
+                return;
+            }
+            const _definitionMap = new Map();
+            const folderPath = Path.dirname(document.uri.fsPath);
+            return new Promise(function (resolve,reject) {
+                let notFound=0;
+                lessFiles.forEach((path) => {
+                    path = path.slice(1, path.length - 1);
+                    path = Path.resolve(folderPath, path);
+                    fs.readFile(path, function (err, data) {
+                        if(err){
+                            reject(e);
+                            return ;
+                        }
+                        const code = data.toString();
+                        
+                        postcss().process(code).then(result => {
+                            let found=false;
+                            result.root.nodes.forEach((node) => {
+                                if (node.selector == currentClass) {
+                                    node.walkRules(rule => {
+                                        const {
+                                            selectors,
+                                            source
+                                        } = rule;
+                                        if (selectors) {
+                                            selectors.forEach(selector => {
+                                                if (selector.slice(1) === currWord) {
+                                                    const definition=getDefinition(Uri.file(path), source);
+                                                    definition.raw=rule.toString();
+                                                    resolve(definition);
+                                                    found=true;
+                                                }
+                                            });
+                                        }
+
+                                    })
+                                }
+                            });
+                            if(!found){
+                                notFound++;
+                                if(notFound===lessFiles.length){
+                                    console.log('reject',notFound);
+                                    reject();
+                                }
+                            }
+                        }).catch(function(e){
+                            reject(e);
+                        });
+                        
+                    });
+                    return path;
+                });
+
+                function getDefinition(path, source) {
+                    const start = new Position(source.start.line - 1, source.start.column);
+                    const end = new Position(source.end.line - 1, source.end.column);
+
+                    return new Location(path, new Range(start, end));
+                }
+            });
+
+            console.log('lessFiles', lessFiles);
+            // const folder=document.uri.fsPath.split('\\\/');
+            // folder.pop();
+
+            console.log(currentClass, currWord, lessFiles);
+        } catch (e) {
+            console.log(e);
+        }
+
+        //   const classAtPosition = '.' + this.getClassAtPosition(document, position, currWordRange, currWord);
+        //   if (this._definitionMap.has(classAtPosition)) {
+        //     return this._definitionMap.get(classAtPosition);
+        //   }
+    }
+
+    // private isClassSelector(selector: string) {
+    //   return selector[0] === '.';
+    // }
+
+    // private getClassAtPosition(document: TextDocument, position: Position, currWordRange: Range, currWord: string): string {
+    //   const classes = _.trim(currWord, `"'`).split(' ');
+    //   const positionOffset = position.character - currWordRange.start.character;
+    //   let startOffset = 0;
+    //   return classes.find(c => {
+    //     startOffset += 1 + c.length;
+    //     return positionOffset <= startOffset;
+    //   });
+    // }
+}
